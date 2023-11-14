e73bc449355b120c0dbc246b30e9deee9fa3034b
Daniil Yankouski <daniil_yankouski@epam.com>
2023-11-14 05:10:53 +0100

    chore(changed): rename app name

---
+++ packages/ReactNativeExampleApp/src/constants/navigation/linking.ts
@@ -0,0 +1,20 @@
+import Config from 'react-native-ultimate-config';
+import { LinkingOptions } from '@react-navigation/native';
+
+import { SCREENS, ScreensParamList } from './screens';
+
+export const linking: LinkingOptions<ScreensParamList> = {
+  prefixes: [Config.LINKING_APP_SCHEME],
+  config: {
+    initialRouteName: SCREENS.Home as const,
+    screens: {
+      Home: {
+        path: SCREENS.Home,
+        screens: {
+          Details: `${SCREENS.Details}/:id`,
+        },
+      },
+      Settings: SCREENS.Settings,
+    },
+  },
+};
